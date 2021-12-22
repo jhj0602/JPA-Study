3f8cdf6476e561aef93c836062550d5a0506290e
jhj960918 <jhj960918@gmail.com>
2021-12-22 23:42:27 +0900

    Modify : 코드 수정

---
+++ src/main/java/com/ssafy/study/embeded/TestUser.java
@@ -0,0 +1,15 @@
+//package com.ssafy.study.embeded;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "user")
+//public class TestUser {
+//
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//
+//    private String name;
+//    
+//}
