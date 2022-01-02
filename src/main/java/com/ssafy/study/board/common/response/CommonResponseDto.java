a71a933fef42c557e59200e8c722f5e46ab96692
jhj960918 <jhj960918@gmail.com>
2022-01-02 23:03:25 +0900

    no message

--- src/main/java/com/ssafy/study/board/common/response/CommonResponseDto.java
+++ src/main/java/com/ssafy/study/board/common/response/CommonResponseDto.java
@@ -1,12 +1,8 @@
 package com.ssafy.study.board.common.response;
 
-import com.ssafy.study.board.enitity.Board;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
-
-import java.util.Optional;
 
 @Getter
 @NoArgsConstructor
