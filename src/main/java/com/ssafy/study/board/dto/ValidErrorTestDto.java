f00c81e29a6902f3bdb93149fca63ea132d526b3
jhj960918 <jhj960918@gmail.com>
2022-01-02 23:04:04 +0900

    Feature : Valid Error Test

---
+++ src/main/java/com/ssafy/study/board/dto/ValidErrorTestDto.java
@@ -0,0 +1,19 @@
+package com.ssafy.study.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@AllArgsConstructor
+public class ValidErrorTestDto {
+
+    @NotBlank(message = "test1 없음")
+    private String test1;
+
+    @NotBlank(message = "test2 없음")
+    @Size(min = 5, max = 10)
+    private String test2;
+}
