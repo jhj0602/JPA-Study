18ca8005f0baf691c518ab1e3627a908c4de4257
jhj960918 <jhj960918@gmail.com>
2022-01-02 23:02:06 +0900

    Feature : 공통 예외 클래스

---
+++ src/main/java/com/ssafy/study/board/common/error/StudyException.java
@@ -0,0 +1,14 @@
+package com.ssafy.study.board.common.error;
+
+import lombok.Getter;
+
+@Getter
+public class StudyException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public StudyException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+}
