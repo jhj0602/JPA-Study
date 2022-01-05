5082d0e0dfc49d4ae93c1ea0aca2f58c3f281811
jhj960918 <jhj960918@gmail.com>
2022-01-06 00:20:27 +0900

    Modify : ManyToOne N+1 문제 해결

--- src/main/java/com/ssafy/study/board/enitity/Board.java
+++ src/main/java/com/ssafy/study/board/enitity/Board.java
@@ -1,28 +1,30 @@
 package com.ssafy.study.board.enitity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.ssafy.study.member.enitity.Member;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "board")
 public class Board {
 
     @Id
-    @GeneratedValue
-    @Column(name = "board_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
     private String content;
 
-
-    
+    @ManyToOne(fetch = FetchType.LAZY)//fetch join N+1문제 해결 방법
     @JoinColumn(name = "member_id")
     private Member member;
 
-
+    @Builder
+    public Board(String content, Member member) {
+        this.content = content;
+        this.member = member;
+    }
 }
