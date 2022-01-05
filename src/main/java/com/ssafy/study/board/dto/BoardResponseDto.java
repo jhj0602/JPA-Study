d6b94ac9f4024a7e238f5bec5c3f7f80bed19fda
jhj960918 <jhj960918@gmail.com>
2022-01-06 00:19:36 +0900

    Feat : Board response

--- src/main/java/com/ssafy/study/board/dto/BoardResponseDto.java
+++ src/main/java/com/ssafy/study/board/dto/BoardResponseDto.java
@@ -1,2 +1,19 @@
-package com.ssafy.study.board.dto;public class BoardResponseDto {
+package com.ssafy.study.board.dto;
+
+import com.ssafy.study.board.enitity.Board;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BoardResponseDto {
+
+    private Long id;
+    private String content;
+    private String userName;
+
+    public static BoardResponseDto from(Board board) { //정적 팩토리 메소드 https://tecoble.techcourse.co.kr/post/2020-05-26-static-factory-method/
+        return new BoardResponseDto(board.getId(), board.getContent(),
+                board.getMember().getUserName());
+    }
 }
