5ea02af9b62db15be4413382bf78526d3317e744
jhj960918 <jhj960918@gmail.com>
2022-01-06 00:22:33 +0900

    Feat : Member enitity 생성

--- src/main/java/com/ssafy/study/member/enitity/Member.java
+++ src/main/java/com/ssafy/study/member/enitity/Member.java
@@ -1,2 +1,30 @@
-package com.ssafy.study.member.enitity;public class Member {
+package com.ssafy.study.member.enitity;
+
+import com.ssafy.study.board.enitity.Board;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    private String userName;
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)//fetch join N+1문제 해결 방법
+    private List<Board> boards = new ArrayList<>();
+
+    @Builder
+    public Member(String userName) {
+        this.userName = userName;
+    }
 }
