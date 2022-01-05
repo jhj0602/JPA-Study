44b193d53e8983e78cc7ab710000a5fb9c28e804
jhj960918 <jhj960918@gmail.com>
2022-01-06 00:20:48 +0900

    Modify : fetch join N+1문제 해결 방법

--- src/main/java/com/ssafy/study/board/enitity/BoardRepository.java
+++ src/main/java/com/ssafy/study/board/enitity/BoardRepository.java
@@ -1,8 +1,19 @@
 package com.ssafy.study.board.enitity;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface BoardRepository extends JpaRepository<Board,Long> {
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    //fetch join N+1문제 해결 방법
+    @EntityGraph(attributePaths = "member")
+    List<Board> findAll();
+
+    @EntityGraph(attributePaths = "member")
+    Optional<Board> findById(Long id);
 }
