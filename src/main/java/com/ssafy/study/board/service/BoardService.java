a11d900b3dc492504f404e0e84950f2788b367e9
jhj960918 <jhj960918@gmail.com>
2022-01-06 00:21:22 +0900

    Feat : service 생성

--- src/main/java/com/ssafy/study/board/service/BoardService.java
+++ src/main/java/com/ssafy/study/board/service/BoardService.java
@@ -1,2 +1,34 @@
-package com.ssafy.study.board.service;public class BoardService {
+package com.ssafy.study.board.service;
+
+import com.ssafy.study.board.dto.BoardResponseDto;
+import com.ssafy.study.board.enitity.Board;
+import com.ssafy.study.board.enitity.BoardRepository;
+import com.ssafy.study.board.exception.NotBoardException;
+import com.ssafy.study.member.enitity.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+    private final MemberRepository memberRepository;
+
+
+    public List<BoardResponseDto> findAll() {
+        List<Board> boards = boardRepository.findAll();
+        return boards.stream().map(BoardResponseDto::from).collect(Collectors.toList());
+    }
+
+    public BoardResponseDto findById(Long id) {
+        Board board = boardRepository.findById(id).orElseThrow(NotBoardException::new);
+        return BoardResponseDto.from(board);
+    }
+
 }
