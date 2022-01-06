ad94aa0d0060fc8c0f9bdd3d0295f2b66075a9a5
jhj960918 <jhj960918@gmail.com>
2022-01-07 00:02:55 +0900

    Feat : email 인증

---
+++ src/main/java/com/ssafy/study/email/EmailController.java
@@ -0,0 +1,35 @@
+package com.ssafy.study.email;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/email") // 이메일 인증 코드 보내기
+    public ResponseEntity<String> emailAuth(@RequestBody Map<String, String> email) throws Exception {
+        emailService.sendSimpleMessage(email.get("email"));
+
+        return new ResponseEntity(true, HttpStatus.OK);
+    }
+
+    @PostMapping("/verifyCode") // 이메일 인증 코드 검증
+    public ResponseEntity<?> verifyCode(@RequestBody Map<String, String> code) {
+        if(EmailService.ePw.equals(code.get("code"))) {
+            return new ResponseEntity(true, HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity(false, HttpStatus.OK);
+        }
+    }
+}
