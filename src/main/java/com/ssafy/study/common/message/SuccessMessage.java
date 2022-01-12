fed08112a7a190bc24042c336045946d4d5843d5
jhj960918 <jjj03231@naver.com>
2022-01-12 22:58:46 +0900

    Feat : SuccessMessage

---
+++ src/main/java/com/ssafy/study/common/message/SuccessMessage.java
@@ -0,0 +1,7 @@
+package com.ssafy.study.common.message;
+
+public class SuccessMessage {
+    public static String SUCCESS_GET_BOARD = "게시판 상세 조회 성공.";
+    public static String SUCCESS_GET_BOARD_LIST = "게시판 리스트 조회 성공.";
+    public static String SUCCESS_GET_BOARD_LIST_USER = "사용자 작성 게시판 리스트 조회 성공.";
+}
