80e05459c3d79de3d536e9dfc8d70faca66fdea9
jhj960918 <jjj03231@naver.com>
2022-01-12 23:01:01 +0900

    Modify : 불필요코드 제거

--- src/main/java/com/ssafy/study/comment/CommentRepository.java
+++ src/main/java/com/ssafy/study/comment/CommentRepository.java
@@ -1,6 +1,5 @@
 package com.ssafy.study.comment;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,10 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment,Long> {
-
-    @EntityGraph(attributePaths = "board")
-    List<Comment> findByBoardId(Long boardId);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query("select c from Comment c join fetch c.member join fetch c.board where c.board.id= :boardId")
     List<Comment> findByJoinBoardId(@Param("boardId") Long boardId);
