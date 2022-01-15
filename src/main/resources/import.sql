72c99709f7ad8b3c509231c2b59fe5042305d2e0
jhj0602 <jhj960918@gmail.com>
2022-01-15 22:42:29 +0900

    Modify : data 변경

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,19 +1,19 @@
 INSERT INTO mbr (user_name) VALUES ('member1');
-INSERT INTO board (content,member_id) VALUES ('content1',1);
-INSERT INTO board (content,member_id) VALUES ('content2',1);
+INSERT INTO board (content,member_id) VALUES ('content1',0,1);
+INSERT INTO board (content,member_id) VALUES ('content2',0,1);
 
 INSERT INTO mbr (user_name) VALUES ('member2');
 INSERT INTO mbr (user_name) VALUES ('member5');
-INSERT INTO board (content,member_id) VALUES ('content3',2);
-INSERT INTO board (content,member_id) VALUES ('content4',2);
+INSERT INTO board (content,views,member_id) VALUES ('content3',0,2);
+INSERT INTO board (content,views,member_id) VALUES ('content4',0,2);
 
 INSERT INTO mbr (user_name) VALUES ('member3');
-INSERT INTO board (content,member_id) VALUES ('content5',3);
-INSERT INTO board (content,member_id) VALUES ('content6',3);
+INSERT INTO board (content,views,member_id) VALUES ('content5',0,3);
+INSERT INTO board (content,views,member_id) VALUES ('content6',0,3);
 
 INSERT INTO mbr (user_name) VALUES ('member4');
-INSERT INTO board (content,member_id) VALUES ('content7',4);
-INSERT INTO board (content,member_id) VALUES ('content8',4);
+INSERT INTO board (content,views,member_id) VALUES ('content7',0,4);
+INSERT INTO board (content,views,member_id) VALUES ('content8',0,4);
 
 
 INSERT INTO comment (content,member_id,board_id) VALUES ('comment1',1,3);
